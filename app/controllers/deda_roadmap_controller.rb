1d86970436abf0f2c5d35500e0a82f2ea19c20d5
Pol1000 <elementale63@hotmail.com>
2012-08-16 14:30:26 +0200

    close issue

--- app/controllers/deda_roadmap_controller.rb
+++ app/controllers/deda_roadmap_controller.rb
@@ -4,15 +4,28 @@ class DedaRoadmapController < ApplicationController
   
   def evade
     issue= Issue.find_by_id(params[:id])
-    testo_nota = params[:id]
+
+    testo_nota = params[:testo]
     if !testo_nota.nil? 
       local_journal = issue.journals.find(:last)
       local_journal.update_attributes(:notes => testo_nota)
+    else
+      local_journal = issue.journals.find(:last)
+      local_journal.update_attributes(:notes => "issue close")
     end
-#    respond_to do |format|
-#        format.html {  redirect_to :controller => 'versions', :action => 'index', :project_id =>params[:project] }
-#   end
-render :update do |page| page << 'window.location.reload()' end
+    stato = nil
+      IssueStatus.find(:all).each do |status|
+          if(status.name == "Evasa")
+            stato = status
+          end
+      end
+      if(stato!=nil)
+          issue.status = stato
+      else
+          issue.status = IssueStatus.create(:name => "Evasa", :is_closed => true)
+    end
+    issue.save
+    render :update do |page| page << 'window.location.reload()' end
   end
   
 
