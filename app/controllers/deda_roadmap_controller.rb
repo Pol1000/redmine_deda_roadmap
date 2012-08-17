f60548e6314acc0b63eb3ffb6fcb27eca6d56032
Pol1000 <elementale63@hotmail.com>
2012-08-17 16:19:30 +0200

    fix bug

--- app/controllers/deda_roadmap_controller.rb
+++ app/controllers/deda_roadmap_controller.rb
@@ -4,31 +4,59 @@ class DedaRoadmapController < ApplicationController
   
   def evade
     issue= Issue.find_by_id(params[:id])
-
-    testo_nota = params[:testo][:name]
     
-    puts testo_nota 
-    if !testo_nota.nil? 
-      local_journal = issue.journals.find(:last)
-      local_journal.update_attributes(:notes => testo_nota)
-    else
-      local_journal = issue.journals.find(:last)
-      local_journal.update_attributes(:notes => "segnalazione chiusa")
+    testo_nota = params[:testo][:name]
+    local_journal = issue.journals.find(:last)
+    
+    if testo_nota.nil? || testo_nota == ''
+      testo_nota = "segnalazione chiusa"
+      
     end
+    local_journal.update_attributes(:notes => testo_nota)
+    
+    
+    
     stato = nil
-      IssueStatus.find(:all).each do |status|
-          if(status.name == "Evasa")
-            stato = status
-          end
+    IssueStatus.find(:all).each do |status|
+      if(status.name == "Evasa")
+        stato = status
       end
-      if(stato!=nil)
-          issue.status = stato
-      else
-          issue.status = IssueStatus.create(:name => "Evasa", :is_closed => true)
+    end
+    if(stato!=nil)
+      issue.status = stato
+    else
+      issue.status = IssueStatus.create(:name => "Evasa", :is_closed => true)
     end
     issue.save
     render :update do |page| page << 'window.location.reload()' end
   end
   
-
+  
+  def respingi
+    issue= Issue.find_by_id(params[:id])
+    
+    testo_nota = params[:testo][:name]
+    local_journal = issue.journals.find(:last)
+    if testo_nota.nil? || testo_nota == ''
+      testo_nota = "segnalazione chiusa"
+      
+    end
+    local_journal.update_attributes(:notes => testo_nota)
+    
+    
+    stato = nil
+    IssueStatus.find(:all).each do |status|
+      if(status.name == "Respinta")
+        stato = status
+      end
+    end
+    if(stato!=nil)
+      issue.status = stato
+    else
+      issue.status = IssueStatus.create(:name => "Respinta", :is_closed => true)
+    end
+    issue.save
+    render :update do |page| page << 'window.location.reload()' end
+  end
+  
 end
