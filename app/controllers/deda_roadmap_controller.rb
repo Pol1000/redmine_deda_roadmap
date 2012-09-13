7d192b87652454a461bd33c2ff51d6c21bab0a2c
Pol1000 <elementale63@hotmail.com>
2012-09-13 11:44:29 +0200

    fix bugs

--- app/controllers/deda_roadmap_controller.rb
+++ app/controllers/deda_roadmap_controller.rb
@@ -6,57 +6,22 @@ class DedaRoadmapController < ApplicationController
     issue= Issue.find_by_id(params[:id])
     
     testo_nota = params[:testo][:name]
+    stato = params[:sel]
     local_journal = issue.journals.find(:last)
     
     if testo_nota.nil? || testo_nota == ''
       testo_nota = "segnalazione chiusa"
       
     end
+    notaLocale = local_journal.notes
+    notaLocale+=testo_nota
     local_journal.update_attributes(:notes => testo_nota)
     
-    
-    
-    stato = nil
-    IssueStatus.find(:all).each do |status|
-      if(status.name == "Evasa")
-        stato = status
-      end
-    end
-    if(stato!=nil)
-      issue.status = stato
-    else
-      issue.status = IssueStatus.create(:name => "Evasa", :is_closed => true)
-    end
+    issue.status = IssueStatus.find_by_id(stato)
     issue.save
     render :update do |page| page << 'window.location.reload()' end
   end
   
   
-  def respingi
-    issue= Issue.find_by_id(params[:id])
-    
-    testo_nota = params[:testo][:name]
-    local_journal = issue.journals.find(:last)
-    if testo_nota.nil? || testo_nota == ''
-      testo_nota = "segnalazione chiusa"
-      
-    end
-    local_journal.update_attributes(:notes => testo_nota)
-    
-    
-    stato = nil
-    IssueStatus.find(:all).each do |status|
-      if(status.name == "Respinta")
-        stato = status
-      end
-    end
-    if(stato!=nil)
-      issue.status = stato
-    else
-      issue.status = IssueStatus.create(:name => "Respinta", :is_closed => true)
-    end
-    issue.save
-    render :update do |page| page << 'window.location.reload()' end
-  end
   
 end
